dc294b4713443dc359591e68adfbf746b6c58036
Andy S Alic <andysalic@upv.es>
2018-10-01 00:23:04 +0200

    upd statusservice

--- deep-orchestrator-plugin/src/main/java/es/upv/indigodc/service/model/DeepDeploymentStatus.java
+++ deep-orchestrator-plugin/src/main/java/es/upv/indigodc/service/model/DeepDeploymentStatus.java
@@ -3,9 +3,10 @@ package es.upv.indigodc.service.model;
 import alien4cloud.paas.model.DeploymentStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 /** Utility class to store mapping between an AlienDeployment and Marathon. */
-@Getter
+@Getter 
 @AllArgsConstructor
 public class DeepDeploymentStatus {
 
@@ -27,6 +28,16 @@ public class DeepDeploymentStatus {
    * launched.
    */
   protected String orchestratorId;
+  
+  /** Error that might be generated when obtaining the status; null means no error. */
+  @Setter
+  protected Throwable error;
+  
   /** The status of the deployment at a given time (when the call to get the info was executed). */
+  @Setter
   protected DeploymentStatus status;
+  
+  public boolean hasError() {
+    return error != null;
+  }
 }
